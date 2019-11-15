d08ac2525f514b537f757464124a35ac10fe460d
jiangmingl <jiangmingl@ip-10-0-1-10.us-west-2.compute.internal>
2019-11-14 21:39:14 -0800

    fix b2 attn bugs

--- allennlp/models/transformer_mc/roberta_models.py
+++ allennlp/models/transformer_mc/roberta_models.py
@@ -1949,7 +1949,7 @@ class RobertaSpanReasoningMultihop3Model(Model):
         B_weights2 = B_weights2.masked_fill((s_masks <= 0), -1e18)
         B_attn2 = torch.softmax(B_weights2, dim=-1)
         output_dict["b_attn2_self"] = B_attn2.masked_fill((s_masks <= 0), 0)
-        B_reps2 = torch.matmul(B_attn2.unsqueeze(1), B_attn2)
+        B_reps2 = torch.matmul(B_attn2.unsqueeze(1), B_reps2)
         # B x 1 x Dim
         B_reps2 = self.dropout(B_reps2)
 
